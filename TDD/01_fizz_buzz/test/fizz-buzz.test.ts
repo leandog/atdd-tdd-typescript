5e0eaa78cc7cd9c673baabf0bab9cfeb8702eef6
Justin Beall <jus.beall@gmail.com>
2021-09-25 21:10:43 -0400

    Add fizz buzz with zombies example

--- TDD/01_fizz_buzz/test/fizz-buzz.test.ts
+++ TDD/01_fizz_buzz/test/fizz-buzz.test.ts
@@ -1,4 +1,20 @@
-import { fizzBuzz } from '../src/fizz-buzz';
+// import { fizzBuzz } from '../src/fizz-buzz';
+
+/*
+# Three Laws of Test Driven Development
+- You are not allowed to write any production code unless it is to make a failing unit test pass.
+- You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
+- You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
+*/
+
+/*
+PROBLEM REQUIREMENTS:
+- For multiples of three print “Fizz” instead of the number
+- For the multiples of five print “Buzz”. 
+- For numbers which are multiples of both three and five print “FizzBuzz“.
+**Upper bound**: 15
+**Example output**: ['1', '2', 'Fizz', '4', 'Buzz', '6', '7', '8', '9', '10', '11', '12', '13', '14', 'FizzBuzz']
+*/
 
 describe('Test Fizz Buzz', () => {
   it('does run Jest', () => {
---
+++ TDD/02_fizz_buzz_zombies/src/fizz-buzz.ts
@@ -0,0 +1,3 @@
+// export function fizzBuzz(upperBound: number): number[] {
+//   return [];
+// }
