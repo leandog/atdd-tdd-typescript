f47c76dbde67e5e58e0899a9d41400caf7802f60
Justin Beall <jus.beall@gmail.com>
2021-09-30 00:08:38 -0400

    Add Tour of Heroes Create

--- Angular/01_hello_world/karma.conf.js
+++ Angular/01_hello_world/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/<project-name>'),
+      dir: require('path').join(__dirname, './coverage/hello-world'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
       check: {
